0e977908eab6f06038f3696496d8eaa0594123f5
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-19 01:29:52 +0500

    Add project files.

---
+++ Tyuiu.GojtievTK.Sprint1.Task0.V8.Test/Test1.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.GojtievTK.Sprint1.Task0.V8.Lib;
+using System;
+namespace Tyuiu.GojtievTK.Sprint1.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.GojtievTK.Sprint1.Task0.V8/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.GojtievTK.Sprint1.Task0.V8.Lib;
+namespace Tyuiu.GojtievTK.Sprint1.Task0.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Гойтиев Т. К. | ПКТб-26-1";
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                            *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                                                     *");
+            Console.WriteLine("* Задание #0                                                                                           *");
+            Console.WriteLine("* Вариант #8                                                                                           *");
+            Console.WriteLine("* Выполнил: Гойтиев Темирсолтан Крымсолтанович | ПКТБ-26-1                                             *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                             *");
+            Console.WriteLine("* Написать консольную программу, которая вычисляет выражение 15/2/4                                    *");
+            Console.WriteLine("* и печатает результат на экране.                                                                      *");
+            Console.WriteLine("*                                                                                                      *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                     *");
+            Console.WriteLine("15/2/4                                                                                                  ");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                           *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+
+
+        }
+    }
+}
