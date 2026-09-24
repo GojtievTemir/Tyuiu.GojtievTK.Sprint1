ac67671ddfc8b9a6972493b1145681f3d4339f24
Темирсолтан Гойтиев <kotik051236@gmail.com>
2026-09-24 23:10:02 +0500

    Task1

---
+++ Tyuiu.GojtievTK.Sprint1.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.GojtievTK.Sprint1.Task0.V25.Lib;
+using System;
+namespace Tyuiu.GojtievTK.Sprint1.Task1.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate(3, 4);
+            Assert.AreEqual(3, res);
+
+
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.GojtievTK.Sprint1.Task1.V8/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.GojtievTK.Sprint1.Task1.V25.Lib;
+namespace Tyuiu.GojtievTK.Sprint1.Task1.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Гойтиев Т. К. | ПКТб-26-1";
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                                            *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                                                     *");
+            Console.WriteLine("* Задание #1                                                                                           *");
+            Console.WriteLine("* Вариант #25                                                                                          *");
+            Console.WriteLine("* Выполнил: Гойтиев Темирсолтан Крымсолтанович | ПКТБ-26-1                                             *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                             *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя  исходные данные                              *");
+            Console.WriteLine("* и вычисляет результат по формуле (x*y)/(1+x) и печатает его на экране.                               *");
+            Console.WriteLine("*                                                                                                      *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                     *");
+            double x, y;
+            Console.WriteLine("Введите значения X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значения Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                           *");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}
